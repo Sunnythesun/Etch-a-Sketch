3b8220a1445253fa26526f6e3234a94a6a4f21df
Sunnythesun <sunnyliang@live.com.au>
2022-04-21 16:53:09 +1000

    event listener

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@
 const body = document.body;
 
 const flexContainer = document.createElement("div");
+
 flexContainer.setAttribute("class", "flex-container");
 body.append(flexContainer);
 
@@ -10,6 +11,7 @@ function createRow() {
 
     for (let i = 0; i < rowSize; i++) {
         const div = document.createElement("div");
+        div.setAttribute("id", "gridDiv");
         flexContainer.append(div);
     }
 }
@@ -23,5 +25,23 @@ function createGrid() {
 }
 
 createGrid();
+
+
+//for mouseover/out colour change
+document.getElementById("gridDiv").addEventListener("mouseover", mouseOver);
+document.getElementById("gridDiv").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    this.style.backgroundColor = "green";
+    console.log("mouse OVER");
+}
+
+function mouseOut() {
+    this.style.backgroundColor = "red";
+    console.log("mouse OUT");
+}
+
+
+
 
 
