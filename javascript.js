edd4781485dacca4dd2cebdaff8528b7ba5bd100
Sunnythesun <sunnyliang@live.com.au>
2022-04-26 23:41:03 +1000

    working grid size

--- javascript.js
+++ javascript.js
@@ -2,19 +2,19 @@
 const body = document.body;
 const flexContainer = document.createElement("div");
 
-//let gridSize = 16;
-
 //to check if grid has been created, so start button can't be used more than once
 let gridCreated = 0;
+
+let gridSize = 0;
 
 flexContainer.setAttribute("id", "flex-container");
 body.append(flexContainer);
 
 
-function createRow() {
+function createRow() {    //not really a row, size set later so it is
     for (let i = 0; i < gridSize; i++) {
         const div = document.createElement("div");
-        div.setAttribute("id", "gridDiv");
+        div.setAttribute("class", "gridDiv");
         flexContainer.append(div);
     }
 }
@@ -24,8 +24,6 @@ function createGrid() {
         createRow();
     }
 }
-
-//createGrid();
 
 //for mouseover/out colour change using bubbling
 function random(number) {
@@ -42,20 +40,29 @@ function onMouse(event) {
 }
 
 //to reset all squares
-
 function reset() {
     location.reload()
 }
 
 function start() {
-    if (gridCreated == 0) {
-        let sizeChoice = prompt("Please enter grid size");
+
+    if (gridCreated === 0) {
+        let width = 0;
+        gridCreated = 1;
+
+        let sizeChoice = prompt("Please enter number of rows/columns");
 
         if (sizeChoice >= 5 && sizeChoice <= 100) {
             gridSize = parseInt(sizeChoice);
-            console.log(gridSize);
+            width = 100 / sizeChoice;
+
+            //create grid with css size then change it when start is pressed
+            let allSquares = document.getElementsByClassName("gridDiv");
             createGrid();
-            gridCreated = 1;
+
+            for (let i = 0; i < allSquares.length; i++) {
+                allSquares[i].style.width = width + "%";
+            }
         }
     }
 }
